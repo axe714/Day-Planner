de831764f66bb4a16e2d8e1b2531f26e3cf6c467
Axe <axearzadon@gmail.com>
2022-04-19 19:40:55 -0700

    Completed saveToDo function, added existingTodo function. Changed color of CSS.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,48 +1,39 @@
 function makeTimeblocks(hour, existingTodo = "") {
     //build some additional logic for if time is past present or future;
-    var currentHour = new Date().getHours();
+    var currentHour = new Date().getHours() - 10;
     var presentPastOrFuture = "future";
     if (currentHour > hour) presentPastOrFuture = "past";
     if (currentHour === hour) presentPastOrFuture = "present";
+    //grabs the the saved todo from local storage
+    var existingTodo = localStorage.getItem(hour);
+    //stops existingTodo from being displaying 'null'
+    if (existingTodo === null) {
+        existingTodo = "";
+    }
     $(".container").append($(`
     <div class="row time-block">
         <div class="hour col-1">${hour}:00</div>
         <textarea name="" id="${hour}" cols="30" rows="3" class="description col-9 ${presentPastOrFuture}">${existingTodo}</textarea>
         <button class="btn saveBtn col-2">Save</button>
     </div>`));
-    //free feel to do the non-jquery equiv.
 }
 
+//for loop to create timeblocks
 for (var i = 9; i < 18; i++) {
     makeTimeblocks(i);
 }
 
+//adds functionality to the save button
 var saveBtn = document.querySelectorAll(".saveBtn")
-var toDoDescription = document.querySelector(".description")
-var toDoHour = document.querySelector(".hour")
 
 // create an event listener for every .saveBtn using for loop
 for (var i = 0; i < saveBtn.length; i++) {
     saveBtn[i].addEventListener("click", saveToDo)
 }
 
+//saves todo to local storage
 function saveToDo(event) {
-    var toDoTime = event.target.parentNode.children[1].value
-    var toDoDescription = event.target.parentNode.children[1].id
-
+    var toDoDescription = event.target.parentNode.children[1].value
+    var toDoTime = event.target.parentNode.children[1].id
+    localStorage.setItem(toDoTime, toDoDescription)
 }
-
-// Not working as intended as of now.
-// function saveToDo(){
-//     localStorage.setItem(toDoHour.textContent, toDoDescription.value);
-// }
-
-// function getToDo (){
-//     toDoDescription.value = localStorage.getItem(toDoHour.textContent);
-// }
-
-// saveBtn.addEventListener("click", saveToDo());
-
-// getToDo();
-
-// when app first loads, need to grab all existing todos and show on the page
